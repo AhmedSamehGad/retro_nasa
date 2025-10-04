c00f5917095333b918086f316caf3500152d24cb
AhmedSamehGad <AhmedSamehGad>
2025-10-04 21:48:49 +0300

    layout_edits

--- front-end/src/components/Layouts/Layout.jsx
+++ front-end/src/components/Layouts/Layout.jsx
@@ -1,24 +1,16 @@
-<<<<<<< HEAD
 import { Outlet, Link } from "react-router-dom"
 import Navbar from "../NavBar"
-=======
-import { Outlet } from "react-router-dom"
->>>>>>> editAuthFE
 
-export default function Layout(){
-    return (
-        <>
-<<<<<<< HEAD
-            <Navbar />
-=======
-            
->>>>>>> editAuthFE
-            <main>
-                <Outlet />
-            </main>
-            <footer>
-                &copy; 2025 Retro Team
-            </footer>
-        </>
-    )
+export default function Layout() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        &copy; 2025 Retro Team
+      </footer>
+    </>
+  )
 }
