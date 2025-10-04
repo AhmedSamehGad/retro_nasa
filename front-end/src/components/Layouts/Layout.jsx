5f902ea5cf5aa056bc60d34ad3105100bc121c0d
AhmedSamehGad <AhmedSamehGad>
2025-10-04 19:32:41 +0300

    navbar

--- front-end/src/components/Layouts/Layout.jsx
+++ front-end/src/components/Layouts/Layout.jsx
@@ -1,18 +1,10 @@
 import { Outlet, Link } from "react-router-dom"
+import Navbar from "../NavBar"
 
 export default function Layout(){
     return (
         <>
-            <header className="p-4 bg-gray-900 text-white">
-                <nav className="max-w-6xl mx-auto flex items-center justify-between">
-                    <h1 className="text-lg font-semibold">Retro NASA</h1>
-                    <div className="space-x-4">
-                        <Link to="/" className="hover:underline">Home</Link>
-                        <Link to="/login" className="hover:underline">Login</Link>
-                        <Link to="/register" className="hover:underline">Register</Link>
-                    </div>
-                </nav>
-            </header>
+            <Navbar />
             <main>
                 <Outlet />
             </main>
